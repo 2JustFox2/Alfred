95b36937eea9e981b80db320bff672a20aa26ded
Sasha <73294558+2JustFox2@users.noreply.github.com>
2025-07-07 19:45:15 +0300

    Delete never used variables

--- src/features/clap-detection/model/clap-detector.service.ts
+++ src/features/clap-detection/model/clap-detector.service.ts
@@ -1,16 +1,5 @@
-function handleDataAvailable(event: BlobEvent) {
-  // console.log("Data available", event);
-  return event;
-}
-
-function handleError(this: MediaRecorder, error: ErrorEvent) {
-  console.error("MediaRecorder error:", error);
-  return error;
-}
-
 async function initMicrophone({
   stream,
-  mediaRecorder,
   isRecording,
   setIsRecording,
   audioContext,
@@ -25,9 +14,6 @@ async function initMicrophone({
         autoGainControl: false,
       },
     });
-    mediaRecorder.current = new MediaRecorder(stream.current);
-    mediaRecorder.current.ondataavailable = handleDataAvailable;
-    mediaRecorder.current.onerror = handleError;
 
     audioContext.current = new AudioContext();
     const source = audioContext.current.createMediaStreamSource(stream.current);
@@ -39,22 +25,16 @@ async function initMicrophone({
     const dataArray = new Float32Array(analyser.fftSize);
     analyser.getFloatFrequencyData(dataArray);
 
-    mediaRecorder.current.onstop = () => {
-      console.log("MediaRecorder stopped");
-      cleanup({ stream, mediaRecorder, isRecording, setIsRecording });
-    };
-
     return analyser;
   } catch (error) {
     console.error("Error initializing microphone:", error);
-    cleanup({ stream, mediaRecorder, isRecording, setIsRecording });
+    cleanup({ stream, isRecording, setIsRecording });
     throw error;
   }
 }
 
 async function startClapDetector({
   stream,
-  mediaRecorder,
   isRecording,
   setIsRecording,
   audioContext,
@@ -72,7 +52,6 @@ async function startClapDetector({
   try {
     const analyser = await initMicrophone({
       stream,
-      mediaRecorder,
       isRecording,
       setIsRecording,
       audioContext,
@@ -123,22 +102,14 @@ async function startClapDetector({
 
     const stopUpdateVolume = setInterval(updateVolume, 100);
     setStopUpdateVolume(stopUpdateVolume);
-
-    if (!mediaRecorder.current) {
-      throw new Error("MediaRecorder not initialized");
-    }
-
-    mediaRecorder.current.start(100); // Записываем 100мс чанки
-    console.log("Recording started", mediaRecorder.current.state);
   } catch (error) {
     console.error("Failed to start listening:", error);
-    cleanup({ stream, mediaRecorder, isRecording, setIsRecording });
+    cleanup({ stream, isRecording, setIsRecording });
   }
 }
 
 async function stopClapDetector({
   stream,
-  mediaRecorder,
   isRecording,
   setIsRecording,
   stopUpdateVolume,
@@ -148,53 +119,26 @@ async function stopClapDetector({
 
   try {
     if (stopUpdateVolume) {
-      console.log(`stopUpdateVolume ${stopUpdateVolume}`);
       clearInterval(stopUpdateVolume);
       setStopUpdateVolume(null);
-    }
-
-    if (!mediaRecorder.current || !stream.current) {
-      throw new Error("MediaRecorder or Stream not initialized");
-    }
-
-    console.log("Stopping with state:", mediaRecorder.current.state);
-
-    if (mediaRecorder.current.state !== "inactive") {
-      mediaRecorder.current.stop();
     }
 
     stream.current.getTracks().forEach((track) => track.stop());
   } catch (error) {
     console.error("Failed to stop listening:", error);
   } finally {
-    cleanup({ stream, mediaRecorder, isRecording, setIsRecording });
+    cleanup({ stream, isRecording, setIsRecording });
   }
 }
 
-function cleanup({ stream, mediaRecorder, isRecording, setIsRecording }) {
+function cleanup({ stream, isRecording, setIsRecording }) {
   if (isRecording) setIsRecording(false);
-
-  if (mediaRecorder.current) {
-    // Удаляем все обработчики
-    mediaRecorder.current.ondataavailable = null;
-    mediaRecorder.current.onerror = null;
-    mediaRecorder.current.onstop = null;
-
-    if (mediaRecorder.current.state !== "inactive") {
-      try {
-        mediaRecorder.current.stop();
-      } catch (e) {
-        console.warn("Error while stopping mediaRecorder:", e);
-      }
-    }
-  }
 
   if (stream.current) {
     stream.current.getTracks().forEach((track) => track.stop());
   }
 
   stream.current = null;
-  mediaRecorder.current = null;
 
   console.log("Cleanup complete");
 }
